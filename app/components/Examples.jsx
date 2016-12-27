e90943c962b37e6c7769a58b5c78496c115420ef
Annie Y <annie_y48@hotmail.com>
2016-12-27 13:50:18 -0800

    Added grid system and style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // stateless function since this simple component only renders to screen
 // can use stateless function because they only define the render method and don't maintain any state
 var Examples = (props) => {
 	return(
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to examples page!</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few example locations to try out:</p>
+			<ol>
+				<li>
+					<Link to='/?location=Salinas'>Salinas, CA</Link>
+				</li>
+				<li>
+					<Link to='/?location=SanFrancisco'>San Francisco, CA</Link>
+				</li>
+			</ol>
 		</div>
 	)
 }
