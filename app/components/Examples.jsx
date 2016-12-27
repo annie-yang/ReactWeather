3f7c20b5e634acc80a96b99974bf393d78e2cf2c
Annie Y <annie_y48@hotmail.com>
2016-12-27 12:42:27 -0800

    renamed example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,10 @@ var React = require('react');
 // can use stateless function because they only define the render method and don't maintain any state
 var Examples = (props) => {
 	return(
-		<h3>Examples Component</h3>
+		<div>
+			<h3>Examples</h3>
+			<p>Welcome to examples page!</p>
+		</div>
 	)
 }
 
