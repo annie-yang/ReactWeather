ada4dc86d0ff7ab69a2555d2afb8b2c903e0a50b
Annie Y <annie_y48@hotmail.com>
2017-01-10 12:29:39 -0800

    Added more comments

--- app/api/OpenWeatherMap.jsx
+++ app/api/OpenWeatherMap.jsx
@@ -5,20 +5,30 @@ var axios = require('axios');
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=f8e48cbded74ae4e049eee0763ffd1e0&units=imperial';
 
 module.exports = {
+	/*
+		getTemp is going to take the location from 'WeatherForm' and return a promise
+		return that data into 'Weather' file
+	*/
 	getTemp: function(location){
+		// takes plain text string and encode it properly for the browser
 		var encodedLocation = encodeURIComponent(location);
 
-		// build URL
-		// query strings
+		/*
+			build URL
+			query strings - build strings by referencing what you want in the URL
+		*/
 		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-		// make request
-		// set up call and handle response
+		/*
+			axios.get makes request by taking URL and fetches it and brings back results
+			set up call and handle response
+		*/
 		return axios.get(requestUrl).then(function(res){
+			// error checking
 			if(res.data.cod && res.data.message){
 				throw new Error(res.data.message);
 			}else{
-				return res.data.main.temp;
+				return res.data.main.temp; // return temperature
 			}
 		}, function(res){
 			throw new Error('Unable to fetch weather for that location.');
