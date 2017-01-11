522455be240bc167f66ebd260a6fa9bc3159ea51
Annie Y <annie_y48@hotmail.com>
2017-01-10 18:39:25 -0800

    Added comment

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
-
-// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
 // redirecting traffic
+// request(index.html, or bundle.js), response (update what we get sent back), next (call when the middleware is done)
 app.use(function(req, res, next){
+	// if traffic is over 'http', if it is, call 'next' to move on
+	// if equal to 'https', redirect them
 	if(req.headers['x-forwarded-proto'] === 'https'){
 		res.redirect('http://' + req.hostname + req.url);
-	}else{
+	}else{ // move on
 		next();
 	}
 });
